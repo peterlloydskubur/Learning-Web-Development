94ed571eeffed6069db8a133df25eddb8f3820d5
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-11 23:08:47 +0300

    Creating a Drum Machine

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -3,7 +3,7 @@ dice1 = Math.floor((Math.random() * 6) + 1);
 dice2 = Math.floor((Math.random() * 6) + 1);
 
 // Chaning image Attribute based on random number
-document.querySelector(".img1").setAttribute("src","images/dice" + dice1 + ".png")
+document.querySelectorAll("img")[0].setAttribute("src","images/dice" + dice1 + ".png")
 document.querySelector(".img2").setAttribute("src","images/dice" + dice2 + ".png")
 
 // Changing the text for the outcome/winner
