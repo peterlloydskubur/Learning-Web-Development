892286e16d2ee071864da81e44f6b8515fe56780
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-12 13:54:01 +0300

    Learning advanced DOM manipulation

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+
+for (let index = 0; index < document.querySelectorAll("button").length; index++) {
+
+    document.querySelectorAll("button")[index].addEventListener("click",function(){
+        alert("I got click");
+    });
+
+}
+
