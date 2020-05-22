3515789353bbda9d1b89703153d3cbfe5e45f989
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-22 16:45:21 +0300

    Calculator with Nodejs and express

--- node-myexpress-server/server.js
+++ node-myexpress-server/server.js
@@ -1,7 +1,22 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/",function(req, res){
+    res.send("Hello world")
+    
+})
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get("/about",function(req, res){
+    console.log("sending info about me");
+    res.send("My name is pedro and I like mac")
+    
+})
+
+
+
+app.listen(port, function(){
+    console.log("Server started in port 3000");
+    
+})
