d1a4254fbdd821592a11daa088b239680c989f3e
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-22 21:36:52 +0300

    bmi cal

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,11 +1,29 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-  });
+    res.sendFile(__dirname + "/index.html");
+
+});
   
-  app.listen(3000, function () {
+app.post("/",function(req,res) {
+
+    var num1 = Number(req.body.n1)
+    var num2 = Number(req.body.n2)
+
+    var sum = num1 / Math.pow(num2 , 2);
+    console.log(Math.pow(num2 , 2));
+    
+    res.send("Your BMI is : "  + sum)
+
+})
+
+
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-  });
+});
   
