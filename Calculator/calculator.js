855052f5de07666049e86a66f6fa034da36cee7a
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-24 19:22:08 +0300

    Learning about API's

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -9,18 +9,34 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 
 });
+
+app.get('/bmiCal', function (req, res) {
+    res.sendFile(__dirname + "/bmiCal.html");
+
+});
   
 app.post("/",function(req,res) {
 
     var num1 = Number(req.body.n1)
     var num2 = Number(req.body.n2)
 
+    var sum = num1 + num2
+    
+    res.send("The sum of these numbers is : "  + sum)
+
+})
+
+app.post("/bmiCal",function(req,res) {
+
+    var num1 = Number(req.body.n3)
+    var num2 = Number(req.body.n4)
+
     var sum = num1 / Math.pow(num2 , 2);
-    console.log(Math.pow(num2 , 2));
     
     res.send("Your BMI is : "  + sum)
 
 })
+
 
 
 app.listen(3000, function () {
