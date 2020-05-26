0c851d1e4fb9fc43a0f3a631c0d20dff04b96361
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-26 23:13:06 +0300

    weather app fixing

--- API-Learning/WeatherAPP/app.js
+++ API-Learning/WeatherAPP/app.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var https = require("https");
 
-var userCity = "rīga";
+var userCity = "berlin";
 
 var app = express();
 
 app.get('/', function (req, res) {
 
-    app.get('/', function(req, resHTML){
-        resHTML.sendFile('index.html');
-    });
+    // res.sendFile(__dirname + '/index.html');
 
     const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.toLocaleLowerCase() + "&appid=a2f9b833a6a185886fc271b42af9257a&units=metric"
 
@@ -17,14 +15,22 @@ app.get('/', function (req, res) {
 
         response.on('data', function (data) {
             const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
+            const temp = Math.round(weatherData.main.temp);
             const weatherDescription = weatherData.weather[0].description
             const country = weatherData.sys.country
             const cityName = weatherData.name
-            res.send(" <h1>The current tempreture for " + country + ", " + cityName + " is " + temp + " degrees in celsius </h1> ")
+            const iconId = weatherData.weather[0].id
+
+            // How do we add the image from the api?
+            const imageURL = "http://openweathermap.org/img/wn/10d@2x.png"
+
+            //res.writeHead(200, { "Content-Type": "text/html" });
+            res.write("<p> The current weather is " + weatherDescription + "</p>");
+            res.write("<h1>Current tempreture for <br>"  + country + ", " + cityName + " is " + temp + " degrees celsius </h1> ");
+
+            res.send();
         });
     });
-
 });
 
 
