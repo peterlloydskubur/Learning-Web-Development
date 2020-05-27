df3ccec9f7c01ba3a56f017e98f12313433ce629
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-27 21:29:27 +0300

    Full weather Job

--- API-Learning/WeatherAPP/app.js
+++ API-Learning/WeatherAPP/app.js
@@ -1,37 +1,51 @@
 var express = require('express');
 var https = require("https");
-
-var userCity = "berlin";
+var bodyParser = require('body-parser')
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.use(bodyParser.urlencoded({extended: true}));
 
-    // res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+
+
+});
+
+app.post('/', function(req,res){
+
+    const userCity = req.body.cityName;
 
     const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.toLocaleLowerCase() + "&appid=a2f9b833a6a185886fc271b42af9257a&units=metric"
 
     https.get(weatherUrl, function (response) {
 
+        
         response.on('data', function (data) {
             const weatherData = JSON.parse(data)
             const temp = Math.round(weatherData.main.temp);
             const weatherDescription = weatherData.weather[0].description
             const country = weatherData.sys.country
             const cityName = weatherData.name
-            const iconId = weatherData.weather[0].id
 
-            // How do we add the image from the api?
-            const imageURL = "http://openweathermap.org/img/wn/10d@2x.png"
+            const IconCode = weatherData.weather[0].icon
+
+            const imageURL = "http://openweathermap.org/img/wn/" + IconCode + "@2x.png"
 
             //res.writeHead(200, { "Content-Type": "text/html" });
+
             res.write("<p> The current weather is " + weatherDescription + "</p>");
             res.write("<h1>Current tempreture for <br>"  + country + ", " + cityName + " is " + temp + " degrees celsius </h1> ");
+            res.write("<img src=" + imageURL+ ">");
 
             res.send();
         });
     });
+        
 });
+
+
+
 
 
 app.listen(3000, function () {
