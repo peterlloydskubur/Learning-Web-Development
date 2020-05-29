12dee6e09d1d389c066890b4cae0a3af03ad4f14
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-30 02:26:05 +0300

    mail champ

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,7 +4,28 @@ var request = require('request');
 
 var app = express()
 
+//Lets use server your css as static from public folder
+app.use(express.static(__dirname + '/public'));
+
+//Adding bodyParser to express
 app.use(bodyParser.urlencoded({ extended: false }))
+
+/
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post('/', function(req,res){
+    const userEmail = req.body.inputEmail;
+    const userName = req.body.name;
+    const userLastName = req.body.lastName;
+
+    console.log("Hello " + userName + " " + userLastName + ", we will be sending emails to: " + userEmail);
+    
+
+
+});
 
 
 app.listen(3000, function () {
