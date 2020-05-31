9e994b7dc048647b14bc04a4d3e99fe9e555046a
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-05-31 03:01:29 +0300

    Finished Newsletter website

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -16,24 +16,13 @@ app.get("/", function (req, res) {
 });
 
 //Reciving information from signup form
+
 app.post('/', function (req, res) {
     const userEmail = req.body.inputEmail;
     const userName = req.body.name;
     const userLastName = req.body.lastName;
 
-    addUserToMailchimp(userName,userLastName,userEmail)
-
-});
-
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
-
-
-function addUserToMailchimp(name,lastname,email) {
-
-    // var request = require('request');
+    // How get this all from app.post and into some sort of function?
     var options = {
         'method': 'POST',
         'url': 'https://us10.api.mailchimp.com/3.0/lists/e4c2ee48be/members/',
@@ -42,12 +31,31 @@ function addUserToMailchimp(name,lastname,email) {
             'Content-Type': 'application/json',
             'Cookie': 'ak_bmsc=D25638FEC6BDBEE26ABD734E0B75E2F1C16C5EA2CA6300009BA1D25E6A20D203~pl+dlnh/qNMte/0YscnzdG3iZ5J6if/efiFzqDafpPy7BUnfwUmLBtIBk/tPNygF/VIpQ7iKA7pqUr88uDQ5DmT3Ut8NDeM+T79g0gsO0fIgMGa0OHDPUC4QhJei6az3V6pFucsaFgQpQZ4JPVh25VODWz1YDVe9XQ/MfDDmSduZteaU/r4v/h7XgjeyKRE3PHL5lqaNa7w7YiYldjhV3eCcVSiaDJSaJoGTzvfsuEMgs=; bm_sv=9DFE614C50B9F307468CAAA70889F33C~RP2oySDGQoeGV7G8nNSV1wiCfzwP38nhgGfSSm65FAmBgJNKyw180vtMousgsBuYnA3lfy3RsXRymZs88hjGkA82BqlYg002AISz1KorY4wQUed+SwJMBfZ4LSQ7VTMKAisGm//TJ8A+VRkJkeNeO3ZrVfJrk96fjriReGAGpDE='
         },
-        body: JSON.stringify({ "list_id": "e4c2ee48be", "email_address": email, "status": "subscribed", "merge_fields": { "FNAME": name, "LNAME": lastname } })
+        body: JSON.stringify({ "list_id": "e4c2ee48be", "email_address": userEmail, "status": "subscribed", "merge_fields": { "FNAME": userName, "LNAME": userLastName } })
 
     };
+
+    // How get this all from app.post and into some sort of function?
     request(options, function (error, response) {
-        if (error) throw new Error(error);
-        console.log("success");
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+            
+        }else{
+            res.sendFile(__dirname + "/failure.html")
+            
+        }
+
+        console.log(response.statusCode);
+        
+        // if (error) throw new Error(error);
     });
 
-}
+
+});
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
