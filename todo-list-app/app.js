a0121ca14472e6fb0bbf8a8aafd89c6f844985fd
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-06-09 18:34:35 +0300

    Added post function to homepage

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -6,28 +6,41 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wenesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
+// Setting how the date will be shown
+var options = {
 
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+
+var today = new Date();
+
+// Calling " today " to show as a string with our " options " object
+console.log(today.toLocaleDateString("de-DE", options));
 
 
 app.get('/', function (req, res) {
 
     res.render("homepage", {
 
-        todaysDate: days[new Date().getDay()]
+        todaysDate: today.toLocaleDateString("de-DE", options)
+
     });
+
+    app.post("/", function (req, res) {
+        console.log(req.body.newTodo);
+    
+    });
+
 });
 
 
+// app.post("/AddToList", function (req, res) {
+//     console.log(req.body.newTodo);
 
+// })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
