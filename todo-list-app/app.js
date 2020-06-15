63257701ff0449a4042d486fd2d3256e70e642c3
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-06-15 23:16:39 +0300

    Added a git ignore and testing some stuff in branch testing1

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -1,58 +1,46 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+// TESTING STUFF IN testing1 branch
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Express use public to enable CSS on localhost
 app.use(express.static(__dirname + '/public'));
 
 // Setting how the date will be shown
 let options = {
-
-    weekday: 'long',
-    // year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+  weekday: 'long',
+  // year: 'numeric',
+  month: 'long',
+  day: 'numeric',
 };
 
-let itemList = []
+let itemList = [];
 
 let today = new Date();
 
-
 app.get('/', function (req, res) {
-
-
-    res.render("homepage", {
-
-        todaysDate: today.toLocaleDateString("de-DE", options),
-        // listTitle: "Some title",
-        itemList: itemList,
-         
-
-    });
-
-
+  res.render('homepage', {
+    todaysDate: today.toLocaleDateString('de-DE', options),
+    // listTitle: "Some title",
+    itemList: itemList,
+  });
 });
 
 // Reciving new information to add on To-Do List
-app.post("/", function (req, res) {
+app.post('/', function (req, res) {
+  //TODO: jQuery submit form when ENTER key is pressed
 
-    //TODO: jQuery submit form when ENTER key is pressed
+  itemList.push(req.body.newItem);
 
-    itemList.push(req.body.newItem)
-
-    // Redirecting back to main page so the new item could be rendered.zz
-    res.redirect("/")
-
-
+  // Redirecting back to main page so the new item could be rendered.zz
+  res.redirect('/');
 });
 
-
-
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
