0b583e903e2651dd8be666ea5dbb2a9ef3956f59
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-06-16 20:03:36 +0300

    Understood more about EJS and added a about.ejs page

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -28,25 +28,36 @@ app.get('/', function (req, res) {
   res.render('homepage', {
     listTitle: todaysDate,
     itemList: itemList,
-    workList: workList,
+    // workList: workList,
   });
 });
 
 // Reciving new information to add on To-Do List
 app.post('/', function (req, res) {
-  itemList.push(req.body.newItem);
+  //* Checking from which url the request came and push accordingly
+  if (req.body.list === 'work') {
+    workList.push(req.body.newItem);
+    // Redirecting back to work page so the new item could be rendered.
+    res.redirect('/work');
+  } else {
+    itemList.push(req.body.newItem);
+    // Redirecting back to main page so the new item could be rendered.
+    res.redirect('/');
+  }
 
-  // Redirecting back to main page so the new item could be rendered.
-  res.redirect('/');
+  console.log(req.body);
 });
 
-// WORK ROUTE //
-
+// Adding work url, stil redirects and check on / route to be added
 app.get('/work', function (req, res) {
   res.render('homepage', {
-    listTitle: 'Work List',
-    workList: workList,
+    listTitle: 'work',
+    itemList: workList,
   });
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
 });
 
 app.listen(3000, function () {
