1c70de6f5dd8ee407e0507422756d26c3cefe717
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-07-06 16:40:22 +0300

    Improving schema

--- FruitsMoongoseLearning/app.js
+++ FruitsMoongoseLearning/app.js
@@ -6,48 +6,38 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', {
   useUnifiedTopology: true,
 });
 
-//Creating database Schema/Layout info
+//Creating database Schema/Layout info with settings
 const fruitLayout = new mongoose.Schema({
-  name: String,
-  rating: Number,
-  review: String,
-  stock: Number,
+  name: {
+    type: String,
+    required: [true, 'Please check your entry, no NAME specified.'],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+    required: [true, 'Please check your entry, no RATING specified.'],
+  },
+  review: {
+    type: String,
+    required: [true, 'Please check your entry, no REVIEW specified.'],
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Please check your entry, no STOCK specified.'],
+  },
 });
 
 // Creating model/collection for fruit db and with Schema "fruitLayout"
 const Fruit = mongoose.model('fruit', fruitLayout);
 
-// Creating new fruit with information, that follows the Fruit layout.
 const fruit = new Fruit({
-  name: 'Apple',
   rating: 7,
-  review: 'Apples are good in the morning',
-  stock: 30,
+  review: 'Something something',
+  stock: 10,
 });
 
-// Creating new fruit with information
-const kiwi = new Fruit({
-  name: 'Kiwi',
-  rating: 8,
-  review: 'Pretty good fruit',
-  stock: 15,
-});
-
-// Creating new fruit with information
-const bannana = new Fruit({
-  name: 'Bannana',
-  rating: 10,
-  review: 'Pretty good with icecream',
-  stock: 15,
-});
-
-// Creating new fruit with information
-const avocado = new Fruit({
-  name: 'Avacado',
-  rating: 9,
-  review: 'Pretty good something',
-  stock: 15,
-});
+fruit.save();
 
 //Creating people Schema
 const personSchema = new mongoose.Schema({
@@ -58,14 +48,7 @@ const personSchema = new mongoose.Schema({
 // Creating model/collection for fruit db with Schema "personSchema"
 const Person = mongoose.model('Person', personSchema);
 
-// Creating new fruit with information, that follows the Fruit layout.
-const person = new Person({
-  name: 'John',
-  age: 22,
-});
-
-// Learning how to read the data.
-
+// Reading and displaying data
 Fruit.find(function (err, fruits) {
   if (err) {
     console.log(err);
