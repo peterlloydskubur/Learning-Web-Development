4541f1e06d5ecfc24a96d958f3c3c3120953ebe5
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-08-17 23:51:32 +0300

    React props practice,added Props default layout

--- keeper-app/src/Components/App.jsx
+++ keeper-app/src/Components/App.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note';
 import Post from './Post';
-
-import './styles.css';
 
 function App() {
   return (
     <div>
-      <h1>My Contacts</h1>
-
+      <h1 className="heading">My Contacts</h1>
       <Post
         name="Beyonce"
-        img="https://www.efusionsoft.com/ready2go/assets/2013/09/500x500-490x490.gif"
+        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
         phone="+1 2323  2313 23 232"
         email="Contatc@email.com"
-      />
-
-      <Post
-        name="Jefferson"
-        img="https://www.efusionsoft.com/ready2go/assets/2013/09/500x500-490x490.gif"
-        phone="+1 999 999 999"
-        email="diffrent@email.com"
-      />
-
-      <Post
-        name="Gunter"
-        img="https://www.agrimaccari.com/en/wp-content/uploads/2015/05/girl-500x500.jpg"
-        phone="+1 888 888 888"
-        email="somuchdiffrent@email.com"
       />
     </div>
   );
--- keeper-app/src/Components/Post.jsx
+++ keeper-app/src/Components/Post.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 function Post(props) {
   return (
-    <div className="post">
-      <h2>{props.name}</h2>
-      <img src={props.img} />
-      <p>{props.phone}</p>
-      <p>{props.email}</p>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
+      </div>
     </div>
   );
 }
