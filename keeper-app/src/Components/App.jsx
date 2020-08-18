6f80dc79e14e1746eeff71229cfdc389c07cc4b2
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-08-18 23:05:53 +0300

    Finished props partice

--- keeper-app/src/Components/App.jsx
+++ keeper-app/src/Components/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Post from './Post';
+import contacts from '../contacts';
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Post
-        name="Beyonce"
-        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        phone="+1 2323  2313 23 232"
-        email="Contatc@email.com"
+        name={contacts[0].name}
+        imgURL={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
+      />
+
+      <Post
+        name={contacts[1].name}
+        imgURL={contacts[1].imgURL}
+        phone={contacts[1].phone}
+        email={contacts[1].email}
+      />
+
+      <Post
+        name={contacts[2].name}
+        imgURL={contacts[2].imgURL}
+        phone={contacts[2].phone}
+        email={contacts[2].email}
       />
     </div>
   );
