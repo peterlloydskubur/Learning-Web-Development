ecc9894618fcf9cbc5c2ce241e5bffcea2deca4c
peterlloydskubur <63150386+peterlloydskubur@users.noreply.github.com>
2020-09-06 22:29:11 +0300

    Designing website Rusteed

--- keeper-app/src/index.js
+++ keeper-app/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import Notes from './notes';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+// Workign design programming Rusteed
